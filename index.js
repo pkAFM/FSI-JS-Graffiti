6bd08b82d64b5d46a90f17abbe418264bbfead0c
pkAFM <pkaur1704@gmail.com>
2022-06-08 19:34:49 -0400

    made changed to js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 let header = document.querySelector('#page-header')
 header.style.textAlign = "left"
+header.style.backgroundColor = "Red"
+
+
+
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+footer.style.backgroundColor = "Red"
 
 
 // Select some elements...
